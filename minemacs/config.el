2c7c10bd389a892195436714e0b2048b08e646d9
lukasz Dechnik <lukasz@dechnik.net>
2023-05-29 13:31:50 +0200

    mu4e-compose-from-mailto function

--- minemacs/config.el
+++ minemacs/config.el
@@ -107,6 +107,38 @@
   (when (file-exists-p "~/.local/share/mail/.emacs/send.el")
     (load-file "~/.local/share/mail/.emacs/send.el")))
 
+(defun mu4e-compose-from-mailto (mailto-string &optional quit-frame-after)
+  (require 'mu4e)
+  (unless mu4e--server-props (mu4e t) (sleep-for 0.1))
+  (let* ((mailto (message-parse-mailto-url mailto-string))
+         (to (cadr (assoc "to" mailto)))
+         (subject (or (cadr (assoc "subject" mailto)) ""))
+         (body (cadr (assoc "body" mailto)))
+         (headers (-filter (lambda (spec) (not (-contains-p '("to" "subject" "body") (car spec)))) mailto)))
+    (when-let ((mu4e-main (get-buffer mu4e-main-buffer-name)))
+      (switch-to-buffer mu4e-main))
+    (mu4e~compose-mail to subject headers)
+    (when body
+      (goto-char (point-min))
+      (if (eq major-mode 'org-msg-edit-mode)
+          (org-msg-goto-body)
+        (mu4e-compose-goto-bottom))
+      (insert body))
+    (goto-char (point-min))
+    (cond ((null to) (search-forward "To: "))
+          ((string= "" subject) (search-forward "Subject: "))
+          (t (if (eq major-mode 'org-msg-edit-mode)
+                 (org-msg-goto-body)
+               (mu4e-compose-goto-bottom))))
+    (font-lock-ensure)
+    (when evil-normal-state-minor-mode
+      (evil-append 1))
+    (when quit-frame-after
+      (add-hook 'kill-buffer-hook
+                `(lambda ()
+                   (when (eq (selected-frame) ,(selected-frame))
+                     (delete-frame)))))))
+
 
 ;; Module: `me-org' -- Package: `org'
 (with-eval-after-load 'org
